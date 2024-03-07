66219a8e9c674e5af5e55bfb508c0229a4a5fb58
aowen7 <aowen@s.sfusd.edu>
2024-03-07 15:42:51 -0800

    attached a script

---
+++ start_menu.gd
@@ -0,0 +1,6 @@
+class_name StartMenu
+
+extends Control
+
+@onready var start_button = $MarginContainer/HBoxContainer/VBoxContainer/StartButton
+@onready var quit_button = $MarginContainer/HBoxContainer/VBoxContainer/QuitButton
