bcfcd9418dc0fa79353843b5ccc6833330345b20
aowen7 <aowen@s.sfusd.edu>
2024-03-13 12:53:01 -0700

    coded start/quit buttons
    start button changes the scene to world scene, quit button closes the game window

--- start_menu.gd
+++ start_menu.gd
@@ -4,3 +4,12 @@ extends Control
 
 @onready var start_button = $MarginContainer/HBoxContainer/VBoxContainer/StartButton
 @onready var quit_button = $MarginContainer/HBoxContainer/VBoxContainer/QuitButton
+
+
+
+func _on_start_button_pressed():
+	get_tree().change_scene_to_file("res://world.tscn")
+
+
+func _on_quit_button_pressed():
+	get_tree().quit()
