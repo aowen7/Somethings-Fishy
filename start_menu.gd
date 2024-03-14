158ced7939d960b6fc1e1eb189b01fc4ca24d5ad
aowen7 <aowen@s.sfusd.edu>
2024-03-14 14:50:49 -0700

    imported start menu background

--- start_menu.gd
+++ start_menu.gd
@@ -8,7 +8,8 @@ extends Control
 
 
 func _on_start_button_pressed():
-	get_tree().change_scene_to_file("res://world.tscn")
+	if Input.is_action_just_pressed("Play"):
+		get_tree().change_scene_to_file("res://world.tscn")
 
 
 func _on_quit_button_pressed():
