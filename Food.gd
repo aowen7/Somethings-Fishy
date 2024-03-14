7ff3a2413c23413151d8ccd1f79a7dd8b8c93446
qusitu1 <122053882+qusitu1@users.noreply.github.com>
2024-03-13 18:37:02 -0700

    bug fixed

--- Food.gd
+++ Food.gd
@@ -18,9 +18,9 @@ func _process(delta):
 			global.is_dragging = false
 			var tween = get_tree().create_tween()
 			if is_inside_dropable:
-				tween.tween_property(self,"position",body_ref.position,0.2).set_ease(tween.EASE_OUT)
+				tween.tween_property(self,"position",body_ref.position,0.2).set_ease(Tween.EASE_OUT)
 			else:
-				tween.tween_property(self,"global_position",body_ref.initialPos,0.2).set_ease(tween.EASE_OUT)
+				tween.tween_property(self,"global_position",initialPos,0.2).set_ease(Tween.EASE_OUT)
 
 func _on_area_2d_mouse_entered():
 	if not global.is_dragging:
@@ -30,15 +30,16 @@ func _on_area_2d_mouse_entered():
 func _on_area_2d_mouse_exited():
 	if not global.is_dragging:
 		draggable = false
-		scale = Vector2(1, 1)
+		scale = Vector2(1,1)
 
 func _on_area_2d_body_entered(body:StaticBody2D):
 	if body.is_in_group('dropable'):
 		is_inside_dropable = true
 		body.modulate = Color(Color.REBECCA_PURPLE, 1)
+		body_ref = body 
 
 func _on_area_2d_body_exited(body):
 	if body.is_in_group('dropable'):
 		is_inside_dropable = false
-		body.modulate = Color(Color.MEDIUM_PURPLE, 0.2)
+		body.modulate = Color(Color.MEDIUM_PURPLE, 0.7)
 	
