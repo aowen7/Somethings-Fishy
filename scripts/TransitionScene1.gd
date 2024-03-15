e2e0b55ea53ad4ee01510d6b9a6c0b5d7ccbf63f
qusitu1 <122053882+qusitu1@users.noreply.github.com>
2024-03-14 18:50:15 -0700

    I.C

--- scripts/TransitionScene1.gd
+++ scripts/TransitionScene1.gd
@@ -9,6 +9,7 @@ func transition():
 	$StartToMainScreen/AnimationPlayer.play("FadeToBlack")
 
 
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
--- scripts/start_menu.gd
+++ scripts/start_menu.gd
@@ -2,15 +2,21 @@ class_name StartMenu
 
 extends Control
 
+signal TransitionScene1
+
 @onready var start_button = $MarginContainer/HBoxContainer/VBoxContainer/StartButton
 @onready var quit_button = $MarginContainer/HBoxContainer/VBoxContainer/QuitButton
 
 
+func _ready():
+		$StartToMainScreen/AnimationPlayer.play("FadeToBlack")
 
 func _on_start_button_pressed():
-	get_tree().change_scene_to_file("res://world.tscn")
+	get_tree().change_scene_to_file("res://scenes/world.tscn")
 	
 
 
 func _on_quit_button_pressed():
 	get_tree().quit()
+
+
