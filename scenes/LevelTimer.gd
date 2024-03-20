2a2f3932884e29d43f4175f06f5266a1b3e96953
desk <dewu@s.sfusd.edu>
2024-03-20 12:55:13 -0700

    5 minute timer

---
+++ scenes/LevelTimer.gd
@@ -0,0 +1,16 @@
+extends Node
+
+@onready var label = $Label
+@onready var timer = $Timer
+
+func _ready():
+	timer.start()
+	
+func time_left_to_live():
+	var time_left = timer.time_left
+	var minute = floor(time_left / 60)
+	var second = int(time_left) % 60
+	return [minute, second]
+	
+func _process(delta):
+	label.text = "%02d:%02d" % time_left_to_live()
