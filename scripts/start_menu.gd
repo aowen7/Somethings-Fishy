9ba347f1b2f3d8c0ee158e39c2b2e65a7dda2a95
qusitu1 <122053882+qusitu1@users.noreply.github.com>
2024-03-20 11:05:22 -0700

    ic

--- scripts/start_menu.gd
+++ scripts/start_menu.gd
@@ -2,6 +2,7 @@ class_name StartMenu
 
 extends Control
 
+
 @onready var start_button = $MarginContainer/HBoxContainer/VBoxContainer/StartButton
 @onready var quit_button = $MarginContainer/HBoxContainer/VBoxContainer/QuitButton
 @onready var scene_manager = $"."
--- scripts/transition_scene.gd
+++ scripts/transition_scene.gd
@@ -4,8 +4,6 @@ signal transitioned
 
 func transition():
 	$AnimationPlayer.play("FadeToBlack")
-	
-
 
 func _on_animation_player_animation_finished(anim_name):
 	if anim_name == "FadeToBlack":
