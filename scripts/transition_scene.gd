6b2adef9aeac97f0b968b51d8a2eff64d2e65748
qusitu1 <122053882+qusitu1@users.noreply.github.com>
2024-03-21 20:21:35 -0700

    fixed part of the transition

--- scripts/transition_scene.gd
+++ scripts/transition_scene.gd
@@ -2,9 +2,12 @@ extends CanvasLayer
 
 signal transitioned
 
+func _ready():
+	transition()
+
 func transition():
 	$AnimationPlayer.play("FadeToBlack")
-
+	
 func _on_animation_player_animation_finished(anim_name):
 	if anim_name == "FadeToBlack":
 		emit_signal("transitioned")
