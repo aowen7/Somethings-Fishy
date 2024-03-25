1cd7a790299a0188a6ea9c71187bda7ac939a1bd
qusitu1 <122053882+qusitu1@users.noreply.github.com>
2024-03-24 18:44:12 -0700

    transition fixed FINALLY

--- scripts/SceneManager.gd
+++ scripts/SceneManager.gd
@@ -4,9 +4,10 @@ extends Node
 const MainScene = preload("res://scenes/world.tscn")
 
 
-func _process(delta):
+func _ready():
 	$TransitionScene.transition() 
 		
 
 func _on_transition_scene_transitioned():
 	get_tree().change_scene_to_file("res://scenes/world.tscn")
+
--- scripts/transition_scene.gd
+++ scripts/transition_scene.gd
@@ -3,14 +3,19 @@ extends CanvasLayer
 signal transitioned
 
 func _ready():
+	$MainGame.visible = false
 	transition()
 
 func transition():
 	$AnimationPlayer.play("FadeToBlack")
 	
+	
 func _on_animation_player_animation_finished(anim_name):
 	if anim_name == "FadeToBlack":
-		emit_signal("transitioned")
 		$AnimationPlayer.play("FadeFromBlack")
-	
+		$StartMenu.visible = false
+		$MainGame.visible = true
+	elif anim_name == "FadeFromBlack":
+		emit_signal("transitioned")
+		
 		
