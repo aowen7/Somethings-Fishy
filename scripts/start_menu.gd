7aa6fb5603dfe918fdf289bbe5de32f1ae3fd1ac
desk <dewu@s.sfusd.edu>
2024-04-17 13:09:42 -0700

    starting menu wave bg

--- scripts/start_menu.gd
+++ scripts/start_menu.gd
@@ -5,8 +5,14 @@ extends Control
 @onready var start_button = $MarginContainer/HBoxContainer/VBoxContainer/StartButton
 @onready var quit_button = $MarginContainer/HBoxContainer/VBoxContainer/QuitButton
 
+@onready var opening_waves = $MusicPack/openingwavesx
+
+func _onready():
+	opening_waves.play()
+	
 func _on_start_button_pressed():
 	get_tree().change_scene_to_file("res://cutscene.tscn")
+	opening_waves.stop()
 
 func _on_quit_button_pressed():
 	get_tree().quit()
