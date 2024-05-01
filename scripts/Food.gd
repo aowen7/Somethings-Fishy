694b0ecfa2119529691e1dcf74a8fff22544a2b7
qusitu1 <122053882+qusitu1@users.noreply.github.com>
2024-05-01 13:29:48 -0700

    fixed the script duplicating thing

--- scripts/Food.gd
+++ scripts/Food.gd
@@ -8,26 +8,33 @@ var initialPos : Vector2
 
 func _process(delta):
 	if draggable:
+		print("it works")
 		if Input.is_action_just_pressed("click"):
 			initialPos = global_position
 			offset = get_global_mouse_position() - global_position
 			global.is_dragging = true
+			
 		if Input.is_action_pressed("click"):
 			global_position = get_global_mouse_position() - offset
+			
 		elif Input.is_action_just_released("click"):
 			global.is_dragging = false
 			var tween = get_tree().create_tween()
+			
 			if is_inside_dropable:
 				tween.tween_property(self,"position",body_ref.position,0.2).set_ease(Tween.EASE_OUT)
+				
 			else:
 				tween.tween_property(self,"global_position",initialPos,0.2).set_ease(Tween.EASE_OUT)
 
 func _on_area_2d_mouse_entered():
+	print("entered")
 	if not global.is_dragging:
 		draggable = true
 		scale = Vector2(1.05, 1.05)
 
 func _on_area_2d_mouse_exited():
+	print("exited")
 	if not global.is_dragging:
 		draggable = false
 		scale = Vector2(1,1)
@@ -38,7 +45,7 @@ func _on_area_2d_body_entered(body:StaticBody2D):
 		body.modulate = Color(Color.REBECCA_PURPLE, 1)
 		body_ref = body 
 
-func _on_area_2d_body_exited(body):
+func _on_area_2d_body_exited(body:StaticBody2D):
 	if body.is_in_group('dropable'):
 		is_inside_dropable = false
 		body.modulate = Color(Color.MEDIUM_PURPLE, 0.7)
