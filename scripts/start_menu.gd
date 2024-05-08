9a4ffc2fffc9115397fe8125216abba77ce2ae94
desk <dewu@s.sfusd.edu>
2024-05-08 13:25:41 -0700

    cutscene update

--- scripts/start_menu.gd
+++ scripts/start_menu.gd
@@ -11,7 +11,7 @@ func _onready():
 	opening_waves.play()
 	
 func _on_start_button_pressed():
-	get_tree().change_scene_to_file("res://cutscene.tscn")
+	get_tree().change_scene_to_file("res://scenes/cutscene.tscn")
 	opening_waves.stop()
 
 func _on_quit_button_pressed():
