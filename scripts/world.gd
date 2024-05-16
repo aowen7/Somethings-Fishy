ee9d61b79316c5c337566924c530d1473b3213bb
desk <dewu@s.sfusd.edu>
2024-05-16 14:51:32 -0700

    customer ghost?

--- scripts/world.gd
+++ scripts/world.gd
@@ -3,36 +3,18 @@ extends Node2D
 @onready var pause_menu = $"PauseMenu"
 @onready var customer_slot_one = $CustomerSlotOne
 
-const CUSTOMER_DELAY = 5  # Delay between customer spawns (in seconds)
-var customer_spawn_timer: float = CUSTOMER_DELAY  # Timer for customer spawn
-
 var customer_scene = preload("res://scenes/customer.tscn")
-var customer_instance = customer_scene.instance()
 
 var paused = false
 
 func _ready():
 	# Start timer for initial customer spawn
 	$Timer.start()
-	
-	print("Customer scene:", customer_scene)  # Print the customer scene path
-	var customer_instance = customer_scene.instance()
-	if customer_instance != null:
-		add_child(customer_instance)
-	else:
-		print("Failed to instance customer scene")
-	
-func _on_Timer_timeout():
-	print("Timer expired")
 	spawn_customer()
+	print("Customer scene:", customer_scene)  # Print the customer scene path
 
 func _process(delta):
-	if customer_spawn_timer <= 0:
-		spawn_customer()
-		customer_spawn_timer = CUSTOMER_DELAY  # Reset timer
 		$Timer.start()
-	else:
-		customer_spawn_timer -= delta
 
 func _input(event):
 	if event.is_action_pressed("pause"):
@@ -41,8 +23,17 @@ func _input(event):
 func spawn_customer():
 	print("Spawning customer")
 	# Instantiate a new customer and add it to the scene
-	var customer = customer_scene.instance()
-	add_child(customer)
+	var customer_instance = customer_scene.instantiate()  # Correct way to instance the scene
+	if customer_instance != null:
+		# Set the position of the customer
+		customer_instance.position = Vector2(-80, 250)  # Example position
+		# Set the Z-index of the customer
+		customer_instance.z_index = -3  # Adjust the Z-index as needed
+		print("Customer position:", customer_instance.position)  # Debugging: Print customer position
+		add_child(customer_instance)
+	else:
+		print("Failed to instance customer scene")
+
 
 func PauseMenu():
 	if paused:
